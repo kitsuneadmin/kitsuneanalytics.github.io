e88440fea2e9d0a98e06702a4cb6ad6b20778b8c
Rex McConnell <rex@rexmac.com>
2018-01-03 23:09:56 -0600

    Updates

--- path---index-cee05b4c142918e3279f.js
+++ path---index-cee05b4c142918e3279f.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{430:function(t,a){t.exports={data:{site:{siteMetadata:{rssMetadata:{title:"Kitsune Analytics"},siteUrl:"https://kitsuneanalytics.com"}}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{426:function(t,a){t.exports={data:{site:{siteMetadata:{rssMetadata:{title:"Kitsune Analytics"},siteUrl:"https://kitsuneanalytics.com"}}},pathContext:{}}}});
 //# sourceMappingURL=path---index-cee05b4c142918e3279f.js.map
